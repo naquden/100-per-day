736ce299a3ad1da8abf5beb73bfce7e7d2409b32
anthony.backenhof <anthony.backenhof@netville.se>
2021-05-04 20:53:19 +0200

    Imported project as jetpack compose

---
+++ app/src/main/java/se/atte/workout/MainActivity.kt
@@ -0,0 +1,38 @@
+package se.atte.workout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import se.atte.workout.ui.theme.WorkoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WorkoutTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WorkoutTheme {
+        Greeting("Android")
+    }
+}
